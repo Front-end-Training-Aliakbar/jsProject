1306bdf1625f65ab7823b048aee57f2166f52975
Nevikai <hasanzadeh.programmer@gmail.com>
2026-05-04 23:30:47 +0330

    day2

---
+++ project1/js/app.js
@@ -0,0 +1,28 @@
+const image = document.querySelector('.profile');
+const menu = document.querySelector('.menu')
+
+if (image && menu) {
+    image.addEventListener('click',function() {
+menu.classList.toggle('active')
+})
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//تمام لیست قابل کلیک می شود
+document.querySelectorAll('.menu ul li').forEach(li => {
+  li.addEventListener('click', () => {
+    li.querySelector('a').click();
+  });
+});
