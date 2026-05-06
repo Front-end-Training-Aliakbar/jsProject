e1adef6d49603cfa83956bbc31508d83c03cccc3
Nevikai <hasanzadeh.programmer@gmail.com>
2026-05-06 23:16:55 +0330

    day3

--- project4/js/app.js
+++ project4/js/app.js
@@ -1,6 +1,10 @@
 const search = document.querySelector(".search");
 const btn = document.querySelector('.btn');
+const input = document.querySelector('input');
 
 btn.addEventListener('click', function () {
-    search.classList.toggle('active')
-})
+    search.classList.toggle('active');
+    if (search.classList.contains('active')) {
+        input.focus();
+    };
+});
