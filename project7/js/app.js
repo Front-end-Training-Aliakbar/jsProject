840142dac4457b14555de82d46ce1bcd7b9c14fc
Nevikai <hasanzadeh.programmer@gmail.com>
2026-05-08 20:03:24 +0330

    day5

---
+++ project7/js/app.js
@@ -0,0 +1,36 @@
+const toTop = document.querySelector('.toTop');
+window.addEventListener('scroll', function () {
+    if (window.pageYOffset >400) {
+        toTop.classList.add('active');
+    }else{
+        toTop.classList.remove('active');
+    }
+})
+
+//scroll smooth
+// toTop.addEventListener('click', function (e) {
+//     e.preventDefault();
+
+//     let scrollStep = -20;
+//     let scrollInterval= setInterval(() => {
+//         if (window.pageYOffset != 0) {
+//             window.scrollBy(0 , scrollStep);
+//         }
+//         else {
+//             clearInterval(scrollInterval)
+//         }
+//     }, 10);
+// })
+toTop.addEventListener('click', function (e) {
+    e.preventDefault;
+
+    function scrollToTop() {
+        if (window.pageYOffset > 0) {
+            window.scrollBy(0 , -20);
+            requestAnimationFrame(scrollToTop)
+        }
+
+    }
+    requestAnimationFrame(scrollToTop)
+})
+
